deb24d3eacf320a69f638ad7317921a9254712b2
Ricky <purujawa06@gmail.com>
2026-03-27 18:42:54 +0700

    Update and rename capacitor.config.ts to capacitor.config.json

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.puruboy.app',
-  appName: 'PuruTV Donghua',
-  webDir: 'www'
-};
-
-export default config;
